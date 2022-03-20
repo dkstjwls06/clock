ad1fa696ec52b8330246330666bfba98e573fc33
dkstjwls06 <love09010224@gmail.com>
2022-03-20 10:55:46 +0900

    idk-why

--- server/src/index.ts
+++ server/src/index.ts
@@ -102,38 +102,52 @@ app.post('/getWhatToDo',async (req,res)=>{
     const now = Number(String(raw._hour)+String(raw._minute))
     const dayOfWeek = req.body.dayOfWeek
     if(!isweek(weekends,dayOfWeek)){
-        if(now>740 && now<2230){
+        if(now>740 && now<2230){//형식 : 할일, 이전 일, 끝난 시간, 다음일, 다음일 시작 시간
             for(let i = 0;i < weekdaystime.length-1;i++){
                 if(now>=weekdaystime[i] && now<weekdaystime[i+1]){
-                    if(i==0) res.json({work:weekdaysTimetable[i],prework:'생활관',start:`${weekdaystime[i]}` ,postwork:weekdaysTimetable[i+1],end:`${weekdaystime[i+1]}`})
-                    //형식 : 할일, 이전 일, 끝난 시간, 다음일, 다음일 시작 시간
-                    else if(i == weekdaystime.length-2){
+                    if(i==0){
+                        res.json({work:weekdaysTimetable[i],prework:'생활관',start:`${weekdaystime[i]}` ,postwork:weekdaysTimetable[i+1],end:`${weekdaystime[i+1]}`})
+                        return
+                    }else if(i == weekdaystime.length-2){
                         res.json({work:weekdaysTimetable[i],prework:weekdaysTimetable[i-1],start:`${weekdaystime[i]}` ,postwork:weekdaysTimetable[i+1],end:`${weekdaystime[i+1]}`})
                         return
                     }
                     res.json({work:weekdaysTimetable[i],prework:weekdaysTimetable[i-1],start:`${weekdaystime[i]}`,postwork:weekdaysTimetable[i+1],end:`${weekdaystime[i+1]}`})
+                    return
                 }
             }
         }else{
-            if(dayOfWeek == '금') res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'810'})
+            if(dayOfWeek == '금'){
+                res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'810'})
+                return
+            }
             res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'740'})
+            return
         }
     }else{
         if(now>810 && now <2230){
             for(let i = 0;i < weekendTime.length-1;i++){
                 if(now>=weekendTime[i] && now<weekendTime[i+1]){
-                    if(i==0) res.json({work:weekendTimetable[i],prework:'생활관',start:`${weekendTime[i]}` ,postwork:weekendTimetable[i+1],end:`${weekendTime[i+1]}`})
-                    //형식 : 할일, 이전 일, 끝난 시간, 다음일, 다음일 시작 시간
-                    else if(i == weekendTime.length-2){
+                    if(i==0) {
+                        res.json({work:weekendTimetable[i],prework:'생활관',start:`${weekendTime[i]}` ,postwork:weekendTimetable[i+1],end:`${weekendTime[i+1]}`})
+                        return
+                    }else if(i == weekendTime.length-2){
                         res.json({work:weekendTimetable[i],prework:weekendTimetable[i-1],start:`${weekendTime[i]}` ,postwork:weekendTimetable[i+1],end:`${weekendTime[i+1]}`})
                         return
                     }
                     res.json({work:weekendTimetable[i],prework:weekendTimetable[i-1],start:`${weekendTime[i]}`,postwork:weekendTimetable[i+1],end:`${weekendTime[i+1]}`})
+                    return
                 }
             }
         }else{
-            if(dayOfWeek == '토') res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'810'})
-            if(dayOfWeek == '일') res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'740'})
+            if(dayOfWeek == '토') {
+                res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'810'})
+                return
+            }
+            if(dayOfWeek == '일') {
+                res.json({work:'생활관',prework:'야간자습 2',start:'2230',postwork:'아침식사',end:'740'})
+                return
+            }
         }
     }
 })
